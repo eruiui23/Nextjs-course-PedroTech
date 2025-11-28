cbeba5f27ba9344d5c0c308065a6f6a05b9ab5c8
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2025-11-28 12:41:14 +0700

    fetch using a funciton and params

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,16 +1,35 @@
-export default async function UsersPage({ params: params }: { params: Promise<{ userId: string }>; }) {
-    const { userId } = await params
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+import { notFound } from "next/navigation";
 
-
+async function fetchUser(id: string) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    if (!res.ok) {
+        return null;
+    }
     const user = await res.json()
+    return user;
+}
 
-    console.log(user)
+function Body({userId, user} : any) {
+    if (!user) {
+        notFound()
+    }
     return (
         <div>
-            <h1>jadi ini itu page use dengan id : { userId }</h1>
+            <h1 style={{color:"red"}}>jadi ini itu page use dengan id : {userId}</h1>
+            <br />
             <p>nama : {user.name}</p>
-
+            <p>email : {user.email}</p>
+            <p>phone : {user.phone}</p>
+            <p>web : {user.website}</p>
         </div>
+    )
+}
+
+export default async function UsersPage({ params: params }: { params: Promise<{ userId: string }>; }) {
+    const { userId } = await params
+    const user = await fetchUser(userId)
+
+    return (
+        <Body userId={userId} user={user}/>
     )
 }
